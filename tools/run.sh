43bb77986ba1e19d44b2eafae98689558a26a34f
Adam Maulana <gitarbetot@gmail.com>
2021-02-13 06:50:38 +0700

    revert

--- tools/run.sh
+++ tools/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-label-studio start ${PROJECT_NAME:-mln_project} -b \
-             --host ${HOST:-localhost} --port ${PORT:-8080} --protocol ${PROTOCOL:-http://} \
+label-studio start ${PROJECT_NAME:-my_project} -b \
+             --host ${HOST:-0.0.0.0} --port ${PORT:-8080} --protocol ${PROTOCOL:-http://} \
              --username ${USERNAME:-""} --password ${PASSWORD:-""} ${INIT_COMMAND:-"--init"}
